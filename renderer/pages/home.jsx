7ec30ef18e2515d9baa28c0d1557dd775c681529
Sam <samrobbinsgb@gmail.com>
2020-07-27 11:28:14 +0100

    Got it working, just need to do styling

--- renderer/pages/home.jsx
+++ renderer/pages/home.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Head from "next/head";
 import Link from "next/link";
+import { useState } from "react";
 var sudo = require("sudo-prompt");
 
 const Home = () => {
+  const [loading, setLoading] = useState(false);
+  const [loaded, setLoaded] = useState(false);
+  const [status, setStatus] = useState("");
+  const [exist, setExist] = useState("");
   function fetchvpro() {
+    setLoading(true);
     console.log("Hello");
     var options = {
       name: "Electron",
@@ -16,7 +22,25 @@ const Home = () => {
       stderr
     ) {
       if (error) throw error;
-      console.log("stdout: " + stdout);
+      const parser = new DOMParser();
+      const xmlDoc = parser
+        .parseFromString(stdout, "text/xml")
+        .getElementsByTagName("Solution");
+      var i;
+      for (i = 0; i < xmlDoc.length; i++) {
+        if (xmlDoc[i].getAttribute("name") === "Intel(R) AMT") {
+          setStatus(xmlDoc[i].getAttribute("state"));
+          setExist(xmlDoc[i].getAttribute("exist"));
+          console.log(xmlDoc[i].getAttribute("state"));
+          console.log(xmlDoc[i].getAttribute("exist"));
+        }
+      }
+      setLoading(false);
+      setLoaded(true);
+      // console.log(
+      //   "stdout: " +
+      //     xmlDoc.getElementsByTagName("Solution")[0].getAttribute("name")
+      // );
     });
   }
 
@@ -38,12 +62,21 @@ const Home = () => {
           </div>
         </div>
         <div className="flex justify-center pt-8">
-          <button
-            className="px-6 py-4 font-semibold text-blue-700 bg-transparent border border-blue-500 rounded hover:bg-blue-500 hover:text-white hover:border-transparent"
-            onClick={fetchvpro}
-          >
-            Get Started
-          </button>
+          {!loading && !loaded && (
+            <button
+              className="px-6 py-4 font-semibold text-blue-700 bg-transparent border border-blue-500 rounded hover:bg-blue-500 hover:text-white hover:border-transparent"
+              onClick={fetchvpro}
+            >
+              Get Started
+            </button>
+          )}
+          {loading && !loaded && <p>Loading</p>}
+          {loaded && (
+            <ul>
+              <li>State: {status}</li>
+              <li>Exist: {exist}</li>
+            </ul>
+          )}
         </div>
       </div>
     </>
