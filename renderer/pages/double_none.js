8fc59fe9f758fb15a0db00113402bb674b6296fd
Sam <samrobbinsgb@gmail.com>
2020-08-07 15:46:09 +0100

    Start work on styling page

--- renderer/pages/double_none.js
+++ renderer/pages/double_none.js
@@ -1,3 +1,18 @@
 export default function Double_None() {
-  return <h1>You don't have anything</h1>;
+  return (
+    <div className="text-center">
+      <h1 className="pt-12 pb-12 text-4xl font-semibold">
+        You don't have Intel vPro
+      </h1>
+      <p className="text-lg">
+        You can find out more about vPro{" "}
+        <a
+          href="https://www.intel.co.uk/content/www/uk/en/architecture-and-technology/vpro/vpro-platform-general.html"
+          className="text-blue-500 underline"
+        >
+          here
+        </a>
+      </p>
+    </div>
+  );
 }
