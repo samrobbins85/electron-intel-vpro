e4dc901e6ef85f3a55464ff769c9de8c2f4cd4b5
Dan Simons <ds00718@surrey.ac.uk>
2021-01-08 14:29:23 +0000

    added double none webpage and changed wording

---
+++ renderer/pages/not_conf.js
@@ -0,0 +1,19 @@
+export default function Double_None() {
+    return (
+      <div className="text-center">
+        <h1 className="pt-12 pb-12 text-4xl font-semibold">
+          This machine is Intel vPro Approved, however AMT is not setup.
+        </h1>
+        <p className="text-lg">
+          To find out what AMT is, and how to set it up, please click {" "}
+          <a
+            href="https://www.intel.co.uk/content/www/uk/en/architecture-and-technology/vpro/vpro-platform-general.html"
+            className="text-blue-500 underline"
+          >
+            here
+          </a>
+        </p>
+      </div>
+    );
+  }
+  
